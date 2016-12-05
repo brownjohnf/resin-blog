555a8100fbba428a708d12168c4a8cdd61767085
John (Jack) Brown <jack@brownjohnf.com>
2016-12-05 09:57:53 -0800

    [Issue #18] Alter blog and Dockerfile to support running on resin
    This change mostly does the following things:
    
    1. Support configuring the database connection in production using
    environment variables, including switching to mysql
    2. Modify links so that they don't have hardcoded paths. This allows
    running the same codebase in multiple environments, and merging them
    together cleanly as improvements are deemed ready for production
    
    Connects to #18
    
    [Issue #18] Consolidate config.js; update index.js; add express
    
    [Issue #18] Clean up formatting; add .dockerenv

---
+++ Makefile
@@ -0,0 +1,19 @@
+IMAGE=localhost/resin-blog:latest
+
+build:
+	docker build -t ${IMAGE} .
+
+run: build
+	docker run -it --rm -p 2368:2368 \
+		--env-file .dockerenv \
+		${IMAGE}
+
+shell: build
+	docker run -it --rm -p 2368:2368 \
+		--env-file .dockerenv \
+		--entrypoint /bin/bash \
+		${IMAGE}
+
+exec:
+	docker exec -it $$(docker ps | grep "${IMAGE}" | cut -f 1 -d ' ') /bin/bash
+
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // # Ghost Startup
 const ghost = require('ghost');
 const path = require('path');
+const express = require('express');
+
+const expressInstance = express();
 
 ghost({
   config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
-  ghostServer.start();
+  expressInstance.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+  ghostServer.start(expressInstance);
 }).catch(function(err) {
   console.error(err);
 });

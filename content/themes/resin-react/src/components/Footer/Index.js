207c3259f053b81b12a279478ad72dd5c7c3085a
gaudi <craig@resin.io>
2017-01-25 16:00:18 +0000

    make components functions

--- content/themes/resin-react/src/components/Footer/Index.js
+++ content/themes/resin-react/src/components/Footer/Index.js
@@ -2,21 +2,19 @@ import React, { Component } from 'react';
 import styles from './style.css';
 
 const Footer = (props) => {
-  render() {
-    return (
-      <footer className={styles.container}>
-          <a className={`${styles.subscribe} icon-feed`} href={`${props.url}/rss/`}><span className={styles.tooltip}>Subscribe!</span></a>
-          <div className={styles.inner}>
-            <section className={styles.copyright}>
-              All content copyright <a href="/">{props.title}</a> &copy; {props.copyright} &bull; All rights reserved.
-            </section>
-            <section className={styles.poweredby}>
-              Proudly published with <a className="icon-ghost" href="http://ghost.org">Ghost</a>
-            </section>
-          </div>
-      </footer>
-    )
-  }
+  return (
+    <footer className={styles.container}>
+        <a className={`${styles.subscribe} icon-feed`} href={`${props.url}/rss/`}><span className={styles.tooltip}>Subscribe!</span></a>
+        <div className={styles.inner}>
+          <section className={styles.copyright}>
+            All content copyright <a href="/">{props.title}</a> &copy; {props.copyright} &bull; All rights reserved.
+          </section>
+          <section className={styles.poweredby}>
+            Proudly published with <a className="icon-ghost" href="http://ghost.org">Ghost</a>
+          </section>
+        </div>
+    </footer>
+  )
 }
 
 export default Footer;
